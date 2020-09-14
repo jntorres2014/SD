7610f00d57b5afe04d92fb41c2606270a2615ad4
jony <jntorres2014@gmail.com>
2020-09-14 15:27:24 -0300

    arreglando

--- p3/A/file_system.py
+++ p3/A/file_system.py
@@ -7,18 +7,30 @@ def list_files(path):
 		print('ERROR!!! ', e)
 		return None
 
-	
-
-def openFile (path):
-	f= open (Path) 
-	return (f) 
-
-
-def readFile (path): 
- 	f=read(Path)
-	return (f) 
-
-def closeFile (path):
-	f=close(Path)
-	return (f) 
+def openFile(path):
+	try:
+		if path not in file_manager:
+		   archivo= open(path)
+		   file_manager.append(archivo)
+		   return archivo
+		return True
+	except Exception as e:
+		print('ERROR!!! ', e)
+		return False
+def readFile(path):
+	try:
+		archivo = read(path)
+		return archivo
+	except Exception as e:
+		print('ERROR!!! ', e)
+		return None
+def closeFile(path):
+	try:
+		if path in file_manager:
+		   close(path)
+		   file_manager.remove(path)
+		   return true
+	except Exception as e:
+		print('ERROR!!! ', e)
+		return False	
 
